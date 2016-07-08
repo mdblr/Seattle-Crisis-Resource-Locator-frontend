63cf02427ed4f1f178b97a2436f4feded3d5ce8b
Max Dobler <mdblr@outlook.com>
2016-07-08 16:31:31 -0700

    start processing data for view

--- features/resource/resource.ctrl.js
+++ features/resource/resource.ctrl.js
@@ -10,16 +10,27 @@
     function ReSrcCtrl(uiGmapGoogleMapApi, ReSrc) {
 
       const vm = this;
+      const services = ReSrc.get();
+
+      vm.views = {
+        food: [],
+        clothing: [],
+        shelter: [],
+        hygiene: [],
+        health: [services.health],
+        human: [services.human],
+        tech: [services.technology]
+      }
+
+      ReSrc.orgMatrServ(services.material, vm.views);
+
       vm.map = {
         center: {
-          latitude: 45,
-          longitude: -73
+          latitude: services.loc_req.lat,
+          longitude: services.loc_req.lng
         },
-        zoom: 8
+        zoom: 16
       };
-
-      vm.resources = ReSrc.get();
-
     }
 
 })();
