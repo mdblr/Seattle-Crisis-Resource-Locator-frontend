793076a143cded9b87980ba07a7bd01a96bd5e83
Max Dobler <mdblr@outlook.com>
2016-07-08 16:34:42 -0700

    introduce parse and orgMatrServ fns to factory

--- features/resource/resource.service.js
+++ features/resource/resource.service.js
@@ -5,24 +5,61 @@
     .module('scrl-app')
     .factory('ReSrc', ReSrc)
 
-    function ReSrc()  {
+  function ReSrc() {
 
-      let resources, tech, hlth, mtrl, shltr, hmn;
+    let resources;
 
-      const store = results => {
-        resources = results;
-      }
-
-      const get = () => {
-        return resources;
-      }
-
-      // const parse = (data) => {
-      //
-      // }
-
-      return { store, get }
+    const store = results => {
+      resources = results;
     }
+
+    const get = () => {
+      return resources;
+    }
+
+    const parse = data => {
+
+      let parsed_data = {
+        loc_req: data.req,
+        technology: [],
+        health: [],
+        material: [],
+        shelter: [],
+        human: []
+      };
+
+      for (let i = 0; i < data.res.length; i++) {
+        data.res[i].map(org => {
+          org.services.forEach(service => {
+            ['technology', 'health', 'material', 'human']
+            .forEach(ctgry => {
+              if (service[ctgry] === true) {
+                ['id', 'name', 'phone_1', 'phone_2', 'address', 'loc_id']
+                .forEach(item => {
+                  service[item] = org[item];
+                });
+                parsed_data[ctgry].push(service);
+              }
+            })
+          })
+        })
+      }
+      return parsed_data;
+    }
+
+    const orgMatrServ = (materialServices, newHolder) => {
+      materialServices.forEach(service => {
+        newHolder[service.subcategory.toLowerCase()].push(service);
+      })
+    }
+
+    return {
+      store,
+      get,
+      parse,
+      orgMatrServ
+    }
+  }
 
 
 })();
