3c5dc957a55adfccac790076f95e88546e2acb68
Max Dobler <mdblr@outlook.com>
2016-07-10 13:18:07 -0700

    make default screen size 1440x900 px

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const {app, BrowserWindow} = electron;
 
 app.on('ready', () => {
   let window = new BrowserWindow({
-    width: 800,
-    height: 600
+    width: 1440,
+    height: 900
   });
   window.loadURL(`file://${__dirname}/index.html`);
 });
