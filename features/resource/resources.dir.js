2551dd76c59e804e26b7127131d408feb097d716
Max Dobler <mdblr@outlook.com>
2016-07-10 17:20:57 -0700

    replace link with controller that changes ng-binding on interval

--- features/resource/resources.dir.js
+++ features/resource/resources.dir.js
@@ -1,51 +1,73 @@
+'use strict';
+
 (function() {
   angular
     .module('scrl-app')
     .directive('dirReSrc', dirReSrc)
 
-    dirReSrc.$inject = ['$interval', '$animate'];
+  dirReSrc.$inject = ['ReSrc', '$interval'];
 
-    function dirReSrc($interval, $animate) {
+  function dirReSrc(ReSrc, $interval) {
 
-      const directive = {
-        restrict: 'E',
-        scope: {
-          object: '='
+    let directive = {
+      restrict: 'E',
+      controller: controller,
+      scope: {
+        title: '=',
+        services: '='
+      },
+      controllerAs: 'vm',
+      templateUrl: 'features/resource/services/services.html'
+    };
+
+    return directive;
+
+    function controller() {
+      let vm = this;
+      const services = ReSrc.getServices();
+      let views = {
+        food: {
+          title: 'Places to Eat',
+          services: []
         },
-        templateUrl: 'features/resource/resources.html',
-        link: link
-      };
-
-      return directive;
-
-      function link() {
-
-        let count = 0;
-        let section;
-        let current;
-
-        $interval(()=> {
-
-          if (current) $animate.addClass(current, 'hide');
-
-          const id = ['food', 'clothing', 'shelter', 'hygiene', 'health', 'human', 'tech'];
-
-          let hasClass = angular.element(document.getElementById(id[count])).hasClass('hide');
-          section = angular.element(document.getElementById(id[count]));
-
-          count++;
-
-          if (count === 7) count = 0;
-
-          if (hasClass === false) {
-            $animate.addClass(section, 'hide');
-          }
-          else if (count > 0 || current) {
-            $animate.removeClass(section, 'hide');
-            current = section;
-          }
-
-        }, 5000);
+        clothing: {
+          title: 'Clothing',
+          services: []
+        },
+        shelter: {
+          title: 'Shelter',
+          services: []
+        },
+        hygiene: {
+          title: 'Hygiene',
+          services: []
+        },
+        health: {
+          title: 'Health',
+          services: services.health
+        },
+        human: {
+          title: 'Resource Centers',
+          services: services.human
+        },
+        tech: {
+          title: 'Internet',
+          services: services.technology
+        }
       }
+
+      vm.view = views['food'];
+
+      ReSrc.orgMatrServ(services.material, views);
+      let count = 1;
+      let cache = {};
+      const keys = Object.keys(views);
+
+      $interval(() => {
+        if (count === 7)  count = 0 ;
+        vm.view = views[keys[count]];
+        count++;
+      }, 20000);
     }
+  }
 })();
