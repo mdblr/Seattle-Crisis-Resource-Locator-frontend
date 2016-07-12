cb80622ab12601fa034ad0dd65c1bec81d6daa36
Max Dobler <mdblr@outlook.com>
2016-07-12 16:58:46 -0700

    send form data to factory

--- features/login/login.service.js
+++ features/login/login.service.js
@@ -5,11 +5,12 @@
     .module('scrl-app')
     .factory('Login', Login)
 
-  Login.$injet = ['http'];
+  Login.$injet = ['http', 'ReSrc'];
 
-  function Login($http) {
+  function Login($http, ReSrc) {
 
     const locate = addr => {
+      ReSrc.saveAddr(addr); 
       return $http({
                 method: 'POST',
                 url: 'http://sea-crisis-resource.herokuapp.com/api/nearby',
