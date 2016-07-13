3ec88ca1521ef6f1f588aaffeca4b9639597a855
Max Dobler <mdblr@outlook.com>
2016-07-12 17:01:35 -0700

    filter for hour data that is 200+ chars

--- features/resource/services/services.service.js
+++ features/resource/services/services.service.js
@@ -8,8 +8,11 @@
       const sliceAt = views => {
         for (view in views) {
           views[view].services.map(service => {
-            if (service.description.length > 300) {
+            if (service.description.length > 250) {
               views[view].sliceAt = 2;
+            }
+            else if (service.hours) {
+              if (service.hours.length > 200) views[view].sliceAt = 2;
             }
           })
         }
