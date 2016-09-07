bf346bd427e8f6e869cff4a7b8f757e768cc6f9c
Max Dobler <mdblr@outlook.com>
2016-09-06 23:39:55 -0700

    update directory

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/features'));
 app.get('/', function(req, res) {
     res.render('index');
 });
